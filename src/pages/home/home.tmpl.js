9c81622f24d68766f7cea232e2bdc463c875296c
avazhov <avazhov@cinimex.ru>
2023-01-17 11:10:32 +0300

    rename tag

--- src/pages/home/home.tmpl.js
+++ src/pages/home/home.tmpl.js
@@ -4,7 +4,7 @@ const HomedPageTemplate = `
         <h1 class="home__title title">
           Главная страница
         </h1>
-         <hav class="home__container">
+         <nav class="home__container">
             <ul class="home__menu">
 							<li class="home__menu-item">
 								<a href="authorization" class="home__box">Авторизация</a>
@@ -25,7 +25,7 @@ const HomedPageTemplate = `
 								<a href="500" class="home__box">500</a>
 							</li>
 						</ul>
-          </hav>
+          </nav>
       </div>
     </main>
 `;
