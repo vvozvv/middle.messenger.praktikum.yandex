51ca1797b818f1da4567d722b3ececaec3bafe2c
vvozvv <a.vvozvv@gmail.com>
2023-02-27 09:55:35 +0300

    Добавил вывод ошибкаи

--- src/utils/checkErrorRequest.ts
+++ src/utils/checkErrorRequest.ts
@@ -10,16 +10,17 @@ import router from "../core/router/Router";
 export const checkErrorRequest = async (request: Promise<any>, goTo?: PAGE) => {
     try {
         const res = await request;
-        const {status} = res as any;
+        const {status, response} = res as any;
 
-        if (status === 200 || status === 401) {
+        if (status === 200) {
           if (goTo) router.go(goTo);
         } else {
-          throw new Error(res)
+          const errorMessage = JSON.parse(response)?.reason ?? 'Ошибка';
+          throw new Error(errorMessage)
         }
-    } catch (e) {
-        console.error('Ошибка', e)
-        alert(e)
+    } catch (error) {
+        console.error('Произошла ошибка', error )
+        alert(error)
     }
 
 }
