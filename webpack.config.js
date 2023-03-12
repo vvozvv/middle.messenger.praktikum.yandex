a0c5128b8c16e0db7b4f689126faf0c3935d009a
avazhov <avazhov@cinimex.ru>
2023-03-12 21:41:45 +0300

    Добавил докеригнор

--- webpack.config.js
+++ webpack.config.js
@@ -42,14 +42,13 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
         ],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
       },
       {
         test: /\.svg$/,
