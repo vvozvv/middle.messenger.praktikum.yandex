5f2b0754776498be4c3a762633c716332bf9c7d1
avazhov <avazhov@cinimex.ru>
2023-03-12 12:33:09 +0300

    Удалил лишнее

--- jest.config.ts
+++ jest.config.ts
@@ -5,10 +5,8 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    'next/router': '<rootDir>/__mocks__/next/router.js',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
     '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': '<rootDir>/__mocks__/file-mock.js',
-    'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
   },
 };
 
