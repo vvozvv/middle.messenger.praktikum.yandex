8b289a60c2b6ee82a97e90d813a5802a13a00a6d
avazhov <avazhov@cinimex.ru>
2023-03-14 21:11:21 +0300

    Фикс webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -13,17 +13,17 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      src: path.resolve(__dirname, '../src'),
+      src: path.resolve(__dirname, './'),
       handlebars: 'handlebars/dist/handlebars.min.js',
-      components: path.resolve(__dirname, '../src/components'),
-      assets: path.resolve(__dirname, '../src/assets'),
-      utils: path.resolve(__dirname, '../src/utils'),
-      store: path.resolve(__dirname, '../src/store'),
-      hoc: path.resolve(__dirname, '../src/hoc'),
-      core: path.resolve(__dirname, '../src/core'),
-      api: path.resolve(__dirname, '../src/api'),
-      modules: path.resolve(__dirname, '../src/modules'),
-      types: path.resolve(__dirname, '../src/core/types'),
+      components: path.resolve(__dirname, '/components'),
+      assets: path.resolve(__dirname, '/assets'),
+      utils: path.resolve(__dirname, '/utils'),
+      store: path.resolve(__dirname, '/store'),
+      hoc: path.resolve(__dirname, '/hoc'),
+      core: path.resolve(__dirname, '/core'),
+      api: path.resolve(__dirname, '/api'),
+      modules: path.resolve(__dirname, '/modules'),
+      types: path.resolve(__dirname, '/core/types'),
     }
   },
   module: {
