99a9c0c8d3cdafa9ec1881dccc35647dce423ba3
avazhov <avazhov@cinimex.ru>
2023-03-15 00:05:28 +0300

    Фикс рендер.ком

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
   mode: 'production',
   optimization: {
+    minimize: true,
     chunkIds: false,
     minimizer: [
       new CssMinimizerPlugin(),
